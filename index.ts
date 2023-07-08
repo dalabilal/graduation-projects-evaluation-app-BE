d8ffb531d04496818581ee6d6e9f6ae05838cbfe
dala_bila <dalahhashlamoon@gmail.com>
2023-07-08 19:41:20 +0300

    store student data

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from "dotenv"
-import { groupRouter, instructorsRouter, quizRouter, studentsNameRouter, studentsRouter, userRouter } from './routers';
+import { groupRouter, instructorsRouter, quizRouter, studentsDataRouter, studentsNameRouter, studentsRouter, userRouter } from './routers';
 import mongoose from 'mongoose';
 import cors from 'cors';
 const app = express();
@@ -23,6 +23,7 @@ app.use('/questions', quizRouter);
 app.use('/users', userRouter);
 app.use('/createGroup', groupRouter);
 app.use('/studentsName',studentsNameRouter);
+app.use('/studentData',studentsDataRouter);
 
 
 const port = 3002;
--- routers/index.ts
+++ routers/index.ts
@@ -4,3 +4,4 @@ export { default as quizRouter } from './questions';
 export { default as userRouter } from './user';
 export { default as groupRouter } from './group';
 export { default as studentsNameRouter } from './students-name';
+export { default as studentsDataRouter } from './student-data';
